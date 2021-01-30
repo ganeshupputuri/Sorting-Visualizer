afaa2dbd0edf407572f1b1bedd1bfa10cabbcd14
ganeshupputuri <ganeshupputuri16@gmail.com>
2021-01-30 14:23:16 +0530

    port changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.use(express.static('static'))
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
